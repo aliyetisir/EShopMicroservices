b8bd6c643631dfe874ce6fe5a0c1b137e72657d0
Ali Yetisir <yetisir@hotmail.com>
2024-05-27 19:46:04 +0200

    Catalog Api implemented

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreteProductHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand
+    (
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+    ) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreteProductHandler(IDocumentSession session) 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // Business locgic to create Product
+        // Save to Database
+        // Return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        session.Store( product );
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductByIdHandler> logger) 
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery qurey, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdHandler.Handler called with {@Query}", qurey);
+        var product = await session.LoadAsync<Product>(qurey.Id, cancellationToken);
+        
+        if (product == null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
