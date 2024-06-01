c145fdee57ab6dac19e36a2436e93de2f08fe8d0
Ali Yetisir <yetisir@hotmail.com>
2024-06-01 16:17:50 +0200

    get basket, delete basket store baket implemented

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,25 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
+builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviors<,>));
+    config.AddOpenBehavior(typeof(LoggingBehaviors<,>));
+});
+
+builder.Services.AddValidatorsFromAssemblies([assembly]);
+
+//builder.Services.AddMarten(opts =>
+//{
+//    opts.Connection(builder.Configuration.GetConnectionString("Database"));
+//})
+//    .UseLightweightSessions();
+
 var app = builder.Build();
 // configure the HTTP request pipeline
+app.MapCarter();
 app.Run();
