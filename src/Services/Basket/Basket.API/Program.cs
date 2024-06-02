3c5f1a612103cde0fe88238d8e40dff039d77272
Ali Yetisir <yetisir@hotmail.com>
2024-06-02 13:04:11 +0200

    Custom exception handling implemented

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
@@ -21,8 +22,10 @@ builder.Services.AddMarten(opts =>
 .UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 // configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 app.Run();
