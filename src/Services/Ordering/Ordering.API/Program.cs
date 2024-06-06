43ac84d77163db9b2948c408508b2db70dbd5aee
Ali Yetisir <yetisir@hotmail.com>
2024-06-06 20:30:30 +0200

    data seed implemented

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
@@ -11,4 +12,10 @@ builder.Services
 
 var app = builder.Build();
 // configure the HTTP request pipeline
+app.UseApiServices();
+
+if(app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 app.Run();
