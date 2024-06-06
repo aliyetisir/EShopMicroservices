fa3e7052d23cf307f747ad6b452125feceeab2c1
Ali Yetisir <yetisir@hotmail.com>
2024-06-06 16:46:29 +0200

    ordering domain implemented

--- src/Services/Ordering/Ordering.Domain/Models/Customerr.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ordering.Domain.Models;
-
-public class Customerr : Entity<CustomerId>
-{
-    public string Name { get; private set; } = default!;
-    public string Email { get; private set; } = default!;
-}
