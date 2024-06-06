3a80ac11490119d5b7949e3df8e6d49ec4d4fae5
Ali Yetisir <yetisir@hotmail.com>
2024-06-06 15:51:36 +0200

    models and value object created

--- src/Services/Ordering/Ordering.Domain/Models/Customerr.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customerr.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class Customerr : Entity<Guid>
+public class Customerr : Entity<CustomerId>
 {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
