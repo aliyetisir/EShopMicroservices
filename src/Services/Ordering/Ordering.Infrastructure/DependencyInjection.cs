ab5eb10a3b811a8c5ad8b5fa38abfa98fe5b3c54
Ali Yetisir <yetisir@hotmail.com>
2024-06-06 18:07:12 +0200

    db initial create migration created

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -8,6 +8,11 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+        services.AddDbContext<ApplicationDbContext, ApplicationDbContext>();
         return services;
     }
 }
